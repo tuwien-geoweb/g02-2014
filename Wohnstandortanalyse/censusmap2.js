4b7864de7411c094beebb928b2b1a6485c632cac
laganna <anna_lagger@gmx.at>
2015-01-14 13:47:05 +0100

    Update censusmap2.js

--- Wohnstandortanalyse/censusmap2.js
+++ Wohnstandortanalyse/censusmap2.js
@@ -66,7 +66,7 @@ olMap.on('singleclick', function(evt) {
   feature.setGeometry(new ol.geom.Point(evt.coordinate));
   feature.set('comment', this.comment.value);
   var xml = new ol.format.WFS().writeTransaction([feature], null, null, {
-    featureType: 'comments', featureNS: 'http://geoweb/2014/g06',
+    featureType: 'comments', featureNS: 'http://geoweb/2014/g02',
     gmlOptions: {srsName: 'EPSG:3857'}
   });
   var xhr = new XMLHttpRequest();
