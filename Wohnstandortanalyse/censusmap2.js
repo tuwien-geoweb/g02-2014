a47700ebf91f981a568b8126b38f828aabf89401
laganna <anna_lagger@gmx.at>
2015-01-23 13:54:59 +0100

    Update censusmap2.js

--- Wohnstandortanalyse/censusmap2.js
+++ Wohnstandortanalyse/censusmap2.js
@@ -116,3 +116,24 @@ form.onsubmit = function(evt) {
           console.log("Accuracy of Geometry: " + geolocation.getAccuracy() + " meters");
       } 
       
+//Variablen laden
+var haltestellen = new ol.layer.Vector({
+  source: new ol.source.GeoJSON({
+  url: 'http://student.ifip.tuwien.ac.at/geoserver/g02_2014/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=g02_2014:g02_2014_Haltestellen&maxFeatures=50&outputFormat=json',
+  projection: 'EPSG:3857'
+}),
+ style: new ol.style.Style({
+       image: new ol.style.Icon({
+          src: 'http://student.ifip.tuwien.ac.at/geoweb/2014/g02/Datensaetze/g02_haltestellen/haltestellen.png',
+        })
+      
+    })
+}); 
+
+document.getElementById('haltestellen').onclick = function(e){
+  if(this.checked==1){
+    olMap.addLayer(haltestellen);
+  }else{
+    olMap.removeLayer(haltestellen);
+  }
+};
