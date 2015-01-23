3b72723dbb70ab2602842eaaa38ca42c08e85ea8
gr2Natasa <gr2Natasa@users.noreply.github.com>
2015-01-23 14:30:27 +0100

    Update censusmap2.js

--- Wohnstandortanalyse/censusmap2.js
+++ Wohnstandortanalyse/censusmap2.js
@@ -124,8 +124,7 @@ var Carsharing = new ol.layer.Vector({
   projection: 'EPSG:3857'
 }),
  style: new ol.style.Style({
-       image: new ol.style.Icon({
-          src: 'http://student.ifip.tuwien.ac.at/geoweb/2014/g02/Datensaetze/g02_carsharing/carsharing.png'})
+       image: new ol.style.Icon({src: 'http://student.ifip.tuwien.ac.at/geoweb/2014/g02/Datensaetze/g02_carsharing/carsharing.png', scale: 0.5 })
     })
 }); 
 
