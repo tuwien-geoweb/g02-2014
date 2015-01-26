3a575936f6f484f3a57fb18d0f3284a2b6303d08
gr2Natasa <gr2Natasa@users.noreply.github.com>
2015-01-26 18:51:15 +0100

    Update censusmap2.js

--- Wohnstandortanalyse/censusmap2.js
+++ Wohnstandortanalyse/censusmap2.js
@@ -137,7 +137,7 @@ document.getElementById('Bezirksgrenzen').onclick = function(e){
  
  var Radwege = new ol.layer.Vector({
   source: new ol.source.GeoJSON({
-  url:'http://student.ifip.tuwien.ac.at/geoserver/g02_2014/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=g02_2014:g02_Radwege&maxFeatures=50000&outputFormat=json',
+  url:'http://student.ifip.tuwien.ac.at/geoserver/g02_2014/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=g02_2014:g02_Radwege&maxFeatures=10000&outputFormat=json',
   projection: 'EPSG:3857'
   }),
 }); 
