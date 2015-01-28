36194664d852db52d9313eeaffa37f341e2c0926
gr2Natasa <gr2Natasa@users.noreply.github.com>
2015-01-28 13:26:50 +0100

    Update censusmap2.js

--- Wohnstandortanalyse/censusmap2.js
+++ Wohnstandortanalyse/censusmap2.js
@@ -158,7 +158,7 @@ var Hundezonen = new ol.layer.Vector({
   projection: 'EPSG:3857'
 }),
  style: new ol.style.Style({
-       image: new ol.style.Icon({src: 'http://student.ifip.tuwien.ac.at/geoweb/2014/g02/Datensaetze/g02_hundezonen/Dog.png', scale: 0.2})
+       image: new ol.style.Icon({src: 'http://student.ifip.tuwien.ac.at/geoweb/2014/g02/Datensaetze/g02_hundezonen/Dog.png', scale: 0.15})
     })
 }); 
 
